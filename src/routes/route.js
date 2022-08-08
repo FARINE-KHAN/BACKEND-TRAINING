c70c3f5c2832cba4a54e1b2e862870e5f2503deb
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 <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>
2022-08-08 16:12:01 +0530

    done

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,21 +1,44 @@
 const express = require('express');
+const lodash = require('lodash');
 // const abc = require('../introduction/intro')
-const a= require('../logger/logger')
-const b=require('../util/helper')
-const c=require('../validator/formatter')
+// const a= require('../logger/logger')
+// const b=require('../util/helper')
+// const c=require('../validator/formatter')
 const router = express.Router();
 
 
 
 router.get('/test-me', function (req, res) {
     res.send('hii,i am farine khan')
-    a.welcome()
-    b.date()
-    b.month()
-    b.batchInfo()
-    c.trim()
-    c.lower()
-    c.upper()
+    // no.1
+    const mon=["jan","feb","mar","apr","may","june","july","aug","sep","oct","nov","dec"]
+    console.log(lodash.chunk(mon, 3))
+
+    // no.2
+
+    const od=[1,3,5,7,9,11,13,15,17,19]
+    console.log(lodash.tail(od));
+
+    //no.3
+
+    const arr=[1,2,3,4,5]
+    console.log(lodash.union(arr,[2,6]));
+
+    //no.4
+
+    const obj =[ ["horror","The Shining"],["drama",'Titanic'],["thriller","Shutter Island"],["fantasy","Pans Labyrinth"]]
+    console.log(lodash.fromPairs(obj));
+
+
+
+
+    // a.welcome()
+    // b.date()
+    // b.month()
+    // b.batchInfo()
+    // c.trim()
+    // c.lower()
+    // c.upper()
 
 
 });
