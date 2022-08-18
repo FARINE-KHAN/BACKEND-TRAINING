72e5f00f75d5ecea80a38c653be3bced1c822199
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 <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>
2022-08-18 23:12:45 +0530

    done

--- src/index.js
+++ src/index.js
@@ -8,14 +8,13 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
-mongoose.connect("mongodb+srv://functionup-cohort:G0Loxqc9wFEGyEeJ@cluster0.rzotr.mongodb.net/Pritesh8769811-DB?retryWrites=true&w=majority", {
+mongoose.connect("mongodb+srv://FARINEKHAN:EXtlAhONzagSoJCy@cluster0.wge8afd.mongodb.net/FARINE09-DB?retryWrites=true&w=majority", {
     useNewUrlParser: true
 })
 .then( () => console.log("MongoDb is connected"))
 .catch ( err => console.log(err) )
 
 app.use('/', route);
-
 
 app.listen(process.env.PORT || 3000, function () {
     console.log('Express app running on port ' + (process.env.PORT || 3000))
