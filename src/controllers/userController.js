168af8558b04dd9d6424eb06a92945ddcbc58ed6
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 <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>
2022-08-25 11:49:52 +0530

    done

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -3,14 +3,12 @@ const UserModel= require("../models/userModel")
 
 
 
-const basicCode= async function(req, res) {
-    let tokenDataInHeaders= req.headers.token
-    console.log(tokenDataInHeaders)
 
-    console.log( "HEADER DATA ABOVE")
-    console.log( "hey man, congrats you have reached the Handler")
-    res.send({ msg: "This is coming from controller (handler)"})
-    }
+
+
+
+
+  
 
 
 
@@ -19,12 +17,14 @@ const basicCode= async function(req, res) {
 
 
 
+// const basicCode= async function(req, res) {
+//     let tokenDataInHeaders= req.headers.token
+//     console.log(tokenDataInHeaders)
 
-
-
-
-
-
+//     console.log( "HEADER DATA ABOVE")
+//     console.log( "hey man, congrats you have reached the Handler")
+//     res.send({ msg: "This is coming from controller (handler)"})
+//     }
 
 
 
